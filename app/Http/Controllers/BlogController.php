7467f684c3138aea382f68a02b44e8f43672ce70
wildan-faris <wildanblogger354@gmail.com>
2023-10-10 00:49:48 +0800

    feat: change controller like heroController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,49 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
-
 class BlogController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $Blog = Blog::all();
-        return view ('admin.blog.index', compact('Blog'));
+        return view('admin.blog.index', compact('Blog'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('admin.blog.tambah');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         if ($request->hasFile('gambar')) {
-            $extention = $request->gambar->extension();
-            $file_name = time() . '.' . $extention;
+            $extension = $request->gambar->extension();
+            $file_name = time() . '.' . $extension;
+            $directory_path = public_path('storage/Blog/');
+
+            if (!File::exists($directory_path)) {
+                File::makeDirectory($directory_path, $mode = 0777, true, true);
+            }
+
             $image = Image::make($request->file('gambar'));
             $image->resize(720, null, function ($constraint) {
                 $constraint->aspectRatio();
             });
-            $image->save(public_path('storage/Blog/' . $file_name), 100);
+            $image->save($directory_path . $file_name, 100);
             $txt = "storage/Blog/" . $file_name;
         } else {
             $txt = null;
@@ -56,41 +46,17 @@ class BlogController extends Controller
             'judul' => $request->judul,
             'isi' => $request->isi,
             'gambar' => $txt
-
         ]);
+
         return redirect()->route('blog.index')->with('success', 'Data Berhasil Ditambahkan');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $Blog = Blog::find($id);
         return view('admin.blog.ubah', compact('Blog'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $Blog = Blog::findOrFail($id);
@@ -98,9 +64,15 @@ class BlogController extends Controller
         $Blog->isi = $request->isi;
 
         if ($request->has("gambar")) {
-            Storage::delete("public/Blog/$Blog->gambar");
-            $extention = $request->gambar->extension();
-            $file_name = time() . '.' . $extention;
+            $current_gambar_path = $Blog->gambar;
+            $extension = $request->gambar->extension();
+            $file_name = time() . '.' . $extension;
+            $directory_path = public_path('storage/Blog/');
+
+            if (!File::exists($directory_path)) {
+                File::makeDirectory($directory_path, $mode = 0777, true, true);
+            }
+
             $txt = "storage/Blog/" . $file_name;
             $image = $request->file('gambar');
             $image = Image::make($request->file('gambar'));
@@ -108,25 +80,22 @@ class BlogController extends Controller
                 $constraint->aspectRatio();
             });
             $image->save(public_path('storage/Blog/' . $file_name), 100);
-        } else {
-            $txt = $Blog->gambar;
+            $Blog->gambar = $txt;
+
+            if ($current_gambar_path && File::exists(public_path($current_gambar_path))) {
+                File::delete(public_path($current_gambar_path));
+            }
         }
-        $Blog->gambar = $txt;
+
         $Blog->save();
         return redirect()->route('blog.index')->with('success', 'Data Berhasil Diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-                $Blog = Blog::findOrFail($id);
-                Storage::delete("public/Blog/$Blog->gambar");
-                $Blog->delete();
-                return redirect()->route('blog.index')->with('success', 'Data Berhasil Dihapus');
+        $Blog = Blog::findOrFail($id);
+        Storage::delete("public/Blog/$Blog->gambar");
+        $Blog->delete();
+        return redirect()->route('blog.index')->with('success', 'Data Berhasil Dihapus');
     }
 }
