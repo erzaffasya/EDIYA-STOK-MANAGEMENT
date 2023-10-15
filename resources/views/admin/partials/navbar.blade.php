1cc07cd5a6383765d4de8adf039b2170ebc72ea4
Erza Fahmi Fasya <58721335+erzaffasya@users.noreply.github.com>
2023-10-15 22:11:00 +0800

    Update navbar.blade.php

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="layout-navbar navbar navbar-expand-xl align-items-center bg-navbar-theme" id="layout-navbar">
-          <div class="container-xxl">
-            <div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
-              <a href="../dashboard" class="app-brand-link gap-2">
+    <div class="container-xxl">
+        <div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
+            <a href="../dashboard" class="app-brand-link gap-2">
                 <span class="app-brand-text demo menu-text fw-bold">Ediya Admin Page</span>
-              </a>
+            </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-xl-none">
                 <i class="mdi mdi-close align-middle"></i>
@@ -16,25 +16,27 @@
             </a>
         </div>
 
-            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-              <ul class="navbar-nav flex-row align-items-center ms-auto">
+        <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
 
                 <!-- User -->
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                    <div class="avatar avatar-online">
-                      <img src="{{asset('tadmin/assets/img/avatars/1.png')}}" alt="profile" class="w-px-40 h-auto rounded-circle" />
-                    </div>
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                        <!-- <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
-                            <i class="mdi mdi-logout me-2 text-danger"></i> Logout
-                            </a> -->
-                    </li>
-                  </ul>
+                    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                        <div class="avatar avatar-online">
+                            <img src="{{ asset('tadmin/assets/img/avatars/1.png') }}" alt="profile"
+                                class="w-px-40 h-auto rounded-circle" />
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    <i class="mdi mdi-logout me-2 text-danger"></i> Logout
+                                </a>
+                        </li>
+                    </ul>
                 </li>
                 <!--/ User -->
             </ul>
